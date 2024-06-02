3a61e6215acb3a2352cfa232353f252b07cc9e28
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-06-02 12:53:54 -0500

    Refactor to load home template on load, edit fonts

--- app/main.py
+++ app/main.py
@@ -8,16 +8,25 @@ PROJECT_BASE_DIR = pathlib.Path(__file__).parent
 STATIC_FILE_DIR = PROJECT_BASE_DIR / "static"
 TEMPLATE_DIR = PROJECT_BASE_DIR / "templates"
 app = FastAPI(
-    title="FastAPI and WebGL fun",    
+    title="Graphical App Catalogue",    
 )
-app.mount(path="/static", app=StaticFiles(directory=STATIC_FILE_DIR), name="static")
 templates = Jinja2Templates(directory=TEMPLATE_DIR)
+
+app.mount(path="/static", app=StaticFiles(directory=STATIC_FILE_DIR), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse(request, name="index.html", context={
         "title": app.title
     })
+
+@app.get("/home", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(request, name="home.html")
+
+@app.get("/about", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse(request, name="about.html")
 
 @app.post("/games", response_class=HTMLResponse)
 async def games(request: Request):
